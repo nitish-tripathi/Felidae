aa871df6501c49b5eb7d059d2b235c40d2545f2e
Nitish Tripathi <nitish@Nitishs-Laptop.local>
2017-02-12 22:17:06 +0100

    [DataProcessing]: Imputing missing values

--- Scikit/DataPreprocessing.py
+++ Scikit/DataPreprocessing.py
@@ -1,6 +1,7 @@
 
 from io import StringIO
 import pandas as pd
+from sklearn.preprocessing import Imputer
 
 def main():
     """ Main """
@@ -12,8 +13,16 @@ def main():
     df_ = pd.read_csv(StringIO(csv_data))
 
     print df_.isnull().sum()
-
     print df_.values
+
+    # Add Missing value using interpolation
+    # Imputer(missing_values='NaN', strategy='mean', axis=0)
+    imr = Imputer()
+    imr = imr.fit(df_)
+    imputed_data = imr.transform(df_.values)
+    print imputed_data
+
+
 
 if __name__ == "__main__":
     main()

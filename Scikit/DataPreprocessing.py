9e23bf623f8b0139f90ecd73013ed322c021f6db
Nitish Tripathi <nitish@Nitishs-Laptop.local>
2017-02-13 22:21:38 +0100

    [DataPreProcessing]: Small Change

--- Scikit/DataPreprocessing.py
+++ Scikit/DataPreprocessing.py
@@ -7,7 +7,7 @@ def main():
     """ Main """
     csv_data = """A,B,C,D
                   1.0,2.0,,4.0
-                  5.0,6.0,,8.0
+                  5.0,6.0,2.3,8.0
                   0.0,,10.0,11.0"""
     csv_data = unicode(csv_data)
     df_ = pd.read_csv(StringIO(csv_data))
